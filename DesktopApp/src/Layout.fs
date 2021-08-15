dc85766aec9a1aa9a9fdc7b882421adee2d77372
Damien Le Berrigaud <damien@initialcapacity.io>
2021-08-14 18:33:04 -0600

    Implement layout for up to 6 builds

--- DesktopApp/src/Layout.fs
+++ DesktopApp/src/Layout.fs
@@ -11,58 +11,74 @@ module Layout =
     let private tooManyReposView =
         TextBlock.subTitle "Too many repos, max is 1." []
 
-    let private one s1 =
+    let private layout1 s1 =
         StatusView.create s1 []
 
-    let private two s1 s2 =
-        Grid.create [
-            Grid.rowDefinitions "*"
-            Grid.columnDefinitions " *, *"
+    let private view s row col =
+        StatusView.create s [ Grid.row row; Grid.column col ]
 
-            Grid.children [
-                StatusView.create s1 [ Grid.column 0 ]
-                StatusView.create s2 [ Grid.column 1 ]
-            ]
+    let private grid rows columns children =
+        let def count = "*" |> List.replicate count |> String.concat ","
+
+        Grid.create [
+            Grid.rowDefinitions (def rows)
+            Grid.columnDefinitions (def columns)
+            Grid.children children
         ] :> IView
 
-    let private three s1 s2 s3 =
-        Grid.create [
-            Grid.rowDefinitions "*"
-            Grid.columnDefinitions "2*, 3*"
-            Grid.children [
-                StatusView.create s1 [ Grid.column 0 ]
-                Grid.create [
-                    Grid.column 1
-                    Grid.rowDefinitions "*, *"
-                    Grid.columnDefinitions "*"
-                    Grid.children [
-                        StatusView.create s2 [ Grid.row 0 ]
-                        StatusView.create s3 [ Grid.row 1 ]
-                    ]
-                ]
-            ]
-        ] :> IView
+    let private layout2 s1 s2 =
+        grid 1 2 [
+            view s1 0 0
+            view s2 0 1
+        ]
 
-    let private four s1 s2 s3 s4 =
-        Grid.create [
-            Grid.rowDefinitions "*, *"
-            Grid.columnDefinitions " *, *"
+    let private grid4 = grid 2 2
 
-            Grid.children [
-                StatusView.create s1 [ Grid.column 0; Grid.row 0 ]
-                StatusView.create s2 [ Grid.column 1; Grid.row 0 ]
-                StatusView.create s3 [ Grid.column 0; Grid.row 1 ]
-                StatusView.create s4 [ Grid.column 1; Grid.row 1 ]
-            ]
-        ] :> IView
+    let private layout3 s1 s2 s3 =
+        grid4 [
+            view s1 0 0
+            view s2 0 1
+            view s3 1 0
+        ]
+
+    let private layout4 s1 s2 s3 s4 =
+        grid4 [
+            view s1 0 0
+            view s2 0 1
+            view s3 1 0
+            view s4 1 1
+        ]
+
+    let private grid6 = grid 2 3
+
+    let private layout5 s1 s2 s3 s4 s5 =
+        grid6 [
+            view s1 0 0
+            view s2 0 1
+            view s3 0 2
+            view s4 1 0
+            view s5 1 1
+        ]
+
+    let private layout6 s1 s2 s3 s4 s5 s6 =
+        grid6 [
+            view s1 0 0
+            view s2 0 1
+            view s3 0 2
+            view s4 1 0
+            view s5 1 1
+            view s6 1 2
+        ]
 
     let create (statuses: BuildStatus list) =
         let view =
             match statuses with
-            | [s1] -> one s1
-            | [s1; s2] -> two s1 s2
-            | [s1; s2; s3] -> three s1 s2 s3
-            | [s1; s2; s3; s4] -> four s1 s2 s3 s4
+            | [s1] -> layout1 s1
+            | [s1; s2] -> layout2 s1 s2
+            | [s1; s2; s3] -> layout3 s1 s2 s3
+            | [s1; s2; s3; s4] -> layout4 s1 s2 s3 s4
+            | [s1; s2; s3; s4; s5] -> layout5 s1 s2 s3 s4 s5
+            | [s1; s2; s3; s4; s5; s6] -> layout6 s1 s2 s3 s4 s5 s6
             | _ -> tooManyReposView
 
         DockPanel.create [
